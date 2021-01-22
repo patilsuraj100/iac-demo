3bad8249d8eb246948526959ea5809244835456f
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-22 19:46:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
-/*provider "aws" {
-  region  = "us-south-1"
+provider "aws" {
+  region     = "p-south-1"
+  access_key = "AKIAI43XAFPN6ZJIXMDQ"
+  secret_key = "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"
 }
-*/

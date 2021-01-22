ee1eecd0089c81c7139e791c59c1da3fdb9e5ada
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-22 23:35:05 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,3 +1,9 @@
+provider"aws"{
+  region = "ap-south-1"
+access_key = "AKIAI43XAFPN6ZJIXMDQ"
+secret_key = "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"
+  }
+
 resource "aws_instance" "AWSServers" {
   ami = "ami-0a9d27a9f4f5c0efc"
   instance_type = "t2.micro"

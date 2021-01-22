421eab0f5a1303911cb360a140d32549f175d845
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-22 23:23:28 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "AWSServers" {
   ami = "ami-0a9d27a9f4f5c0efc"
   instance_type = "t2.micro"
-  Key_name = "test_key"
+  Key_name = "new-key"
   security_groups =["lanuch-wizard-19"]
   tags = {
     Name = "terraform-server"

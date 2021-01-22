08ef6a6fc2efe4ba0f25b8efd8bdf8354c7d034c
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-22 23:38:56 +0530

    Delete EC2.tf

--- EC2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "AWSServers" {
-  ami = "ami-0a9d27a9f4f5c0efc"
-  instance_type = "t2.micro"
-  Key_name = "Test_Key"
-  security_groups =["lanuch-wizard-19"]
-  tags = {
-    Name = "terraform-server"
-  }
-}

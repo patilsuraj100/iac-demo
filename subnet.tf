945c169f0599728e63954a59ef00a3960f2f0936
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-22 20:33:24 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -6,11 +6,3 @@ resource "aws_subnet" "main" {
     Name = "Main"
   }
 }
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}

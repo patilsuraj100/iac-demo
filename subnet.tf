d6e45d0642612285a7a7dcc1f3152b4a48a50275
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-23 09:27:52 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,7 @@
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
+  cidr_block = "10.0.0.0/24"
   tags = {
-    Name = "Main"
+    Name = "MySubnet"
   }
 }

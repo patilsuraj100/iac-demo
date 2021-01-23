6654d718e6d4c2ce6b1b298962fb1805af33d82b
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-23 15:37:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_network_interface" "test" {
   subnet_id       = aws_subnet.subnet1.id
-  private_ips     = ["10.0.0.50"]
+  private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_tls.id]
 }
 

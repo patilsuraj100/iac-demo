e68f45c933931fb223ccb51f2112286064dd9121
patilsuraj100 <73334873+patilsuraj100@users.noreply.github.com>
2021-01-23 15:21:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -24,7 +24,7 @@ resource "aws_route_table" "r" {
     ipv6_cidr_block        = "::/0"
     gateway_id = aws_internet_gateway.gw.id
 }
-
+	}
 resources "aws_subnet" "subnet1" {
 	vpc_id = aws_vpc.main.id
 	cidr_block = "10.0.1.0/24"
